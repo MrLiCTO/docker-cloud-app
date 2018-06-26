13745f269621c0b015c25eb8712a6e77a964928e
李世龙 <lishilong@ablecloud.cn>
2018-06-26 19:13:34 +0800

    修改pom文件

---
+++ app-monitor/src/main/java/com/wxcs/monitor/app/AppMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.wxcs.monitor.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMonitorApplication.class, args);
+	}
+}
---
+++ app-register/src/main/java/com/wxcs/register/app/AppRegisterApplication.java
@@ -0,0 +1,12 @@
+package com.wxcs.register.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRegisterApplication.class, args);
+	}
+}
